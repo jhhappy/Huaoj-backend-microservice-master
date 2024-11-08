d969528538c63cf4cabb7424273cd78a4b8a3883
JinjH <12701845+jhhappy@user.noreply.gitee.com>
2024-11-08 16:44:11 +0800

    测试提交

--- Huaoj-backend-gateway/src/main/java/com/JinjH/Huaojbackendgateway/filter/GlobalAuthFilter.java
+++ Huaoj-backend-gateway/src/main/java/com/JinjH/Huaojbackendgateway/filter/GlobalAuthFilter.java
@@ -33,6 +33,7 @@ public class GlobalAuthFilter implements GlobalFilter, Ordered {
             return response.writeWith(Mono.just(dataBuffer));
         }
         // todo 统一权限校验，通过 JWT 获取登录用户信息
+        //test
         return chain.filter(exchange);
     }
 
